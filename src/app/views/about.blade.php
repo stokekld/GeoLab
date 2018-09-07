d1432d0967075449a4129cb6e603740a9c76edfa
Jesus Flores <joy_warmgun@hotmail.com>
2018-09-07 14:17:39 -0500

    Cambiando contenido de acerca de  On branch master 	modified:   src/app/views/about.blade.php

--- src/app/views/about.blade.php
+++ src/app/views/about.blade.php
@@ -19,137 +19,16 @@
                      <!-- Service hero -->
                      <div class="hero">
                         <!-- Title. Don't forget the <span> tag -->
-                        <h3><span>Service</span></h3>
+                        <h3><span>Manual de prácticas de laboratorio para el procesamiento de imágenes de satélite y análisis de datos geoespaciales (PE110218)</span></h3>
                         <!-- para -->
-                        <p>Praesent at tellus porttitor nisl porttitor sagittis. Mauris in massa ligula, a tempor nulla. Ut tempus interdum mauris vel vehicula. Nulla ullamcorper tortor commodo in sagittis est accumsan.</p>
+			<p>Este manual de prácticas se ha podido realizar gracias al Programa de Apoyo a Proyectos para la Innovación y Mejoramiento de la Enseñanza, (PAPIME) 2018. El objetivo primordial de los proyectos PAPIME es impulsar la superación y el desarrollo del personal académico, de la Universidad Nacional Autónoma de México (UNAM).</p>
+			<p>Los colaboradores de este proyecto estamos seguros de que el manual de prácticas de laboratorio es una herramienta muy eficaz para la transmisión del conocimiento, ya que en él se tiene una versión simplificada de los trabajos de investigación, tales como: tesis, tesinas, reportes técnicos, reportes de servicios sociales, e investigaciones de interés a nivel nacional.</p>
+			<p>Enfatizamos el uso de software libre y licencias de bajo costo para los estudiantes y académicos, con el fin de tener herramientas tecnológicas útiles y al margen de lo legal. Muchas de las metodologías aquí presentadas han sido realizadas primero en software privativo y de alto costo y nos hemos dado a la tarea de cambiarlas a software libre o de bajo costo.</p>
+			<p>Los colaboradores han querido destacar los trabajos y logros que se tienen a nivel nacional entre las instituciones que realizan una labor científica, y de investigación, en las diversas áreas de interés para la ingeniería aplicada. Utilizando metodologías cuya eficacia en tiempo, minimiza costos en las investigaciones, característica de vital importancia para quienes trabajamos en las academias (estudiantes, profesores e investigadores).</p>
+			<p>Además de las prácticas, se encuentran también algunos de los insumos utilizados para realizarlas, de tal manera que se pueden realizar las metodologías para comprobar la eficacia de los manuales.</p>
+			<p>Esperamos este recurso sea de interés para la comunidad estudiantil y académica, agradecemos el apoyo a la División de Ingenierías Civil y Geomática, al Programa de Apoyo a Proyectos para la Innovación y Mejoramiento de la Enseñanza, así como a los colaboradores, tesistas y becarios que hicieron posible este trabajo.</p>
                      </div>
                      <!-- Service -->
-                     
-                     <!-- Service list. Note down the class name before editing. -->
-                     <div class="row">
-                        <div class="col-md-6 col-sm-6">
-                           <div class="aservy">
-                              <h4>What we offer?</h4>
-                              <p>Aliquam consequat, purus vitae auctor ullamcorper, sem velit convallis quam, a pharetra justo nunc et mauris. Vivamus diam diam, fermentum sed dapibus eget, egestas sed eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                              <div class="aserv-list">
-                                 <div class="aserv-l">
-                                    
-                                    <!-- Service #1 -->
-                                    <div class="aserv">
-                                       <!-- Font awesome icon -->
-                                       <div class="aserv-img">
-                                          <i class="fa fa-gift"></i>
-                                       </div>
-                                       <!-- Detail -->
-                                       <div class="aserv-details">
-                                          Fertum <span class="color">sed dapibus</span>
-                                       </div>
-                                       <div class="clearfix"></div>
-                                    </div>
-                                    
-                                    <div class="aserv">
-                                       <div class="aserv-img">
-                                          <i class="fa fa-globe"></i>
-                                       </div>
-                                       <div class="aserv-details">
-                                          Egestas <span class="color">sed dapibus</span>
-                                       </div>
-                                       <div class="clearfix"></div>
-                                    </div>
-
-                                    <div class="aserv">
-                                       <div class="aserv-img">
-                                          <i class="fa fa-truck"></i>
-                                       </div>
-                                       <div class="aserv-details">
-                                          Dapibus <span class="color">sed dapibus</span>
-                                       </div>
-                                       <div class="clearfix"></div>
-                                    </div>
-
-                                    <div class="aserv">
-                                       <div class="aserv-img">
-                                          <i class="fa fa-filter"></i>
-                                       </div>
-                                       <div class="aserv-details">
-                                          Ut teus <span class="color">sed dapibus</span>
-                                       </div>
-                                       <div class="clearfix"></div>
-                                    </div>                                    
-                                    
-                                 </div>
-                                 <div class="aserv-r">
-                                    <div class="aserv">
-                                       <div class="aserv-img">
-                                          <i class="fa fa-tag"></i>
-                                       </div>
-                                       <div class="aserv-details">
-                                          Consetur <span class="color">sed dapibus</span>
-                                       </div>
-                                       <div class="clearfix"></div>
-                                    </div>
-                                    
-                                    <div class="aserv">
-                                       <div class="aserv-img">
-                                          <i class="fa fa-flask"></i>
-                                       </div>
-                                       <div class="aserv-details">
-                                          Aliquam <span class="color">sed dapibus</span>
-                                       </div>
-                                       <div class="clearfix"></div>
-                                    </div>
-
-                                    <div class="aserv">
-                                       <div class="aserv-img">
-                                          <i class="fa fa-fire"></i>
-                                       </div>
-                                       <div class="aserv-details">
-                                          Vivamus <span class="color">sed dapibus</span>
-                                       </div>
-                                       <div class="clearfix"></div>
-                                    </div>
-
-                                    <div class="aserv">
-                                       <div class="aserv-img">
-                                          <i class="fa fa-thumb-tack"></i>
-                                       </div>
-                                       <div class="aserv-details">
-                                          Fertum <span class="color">sed dapibus</span>
-                                       </div>
-                                       <div class="clearfix"></div>
-                                    </div>                                     
-                                 </div>    
-                                 <div class="clearfix"></div>
-                              </div>
-                           </div>  
-                           <div class="clearfix"></div>                    
-                        </div>                        
-                        <div class="col-md-6 col-sm-6">
-                           <div class="well">
-                              <h5>Vivamus vel</h5>
-                              <p>Suspendisse potenti. Morbi ac felis nec mauris imperdiet fermentum. Aenean sodales augue ac lacus hendrerit sed rhoncus erat hendrerit. Vivamus vel ultricies elit. Aliquam ut feugiat ante. Curabitur lacinia nulla vel tellus elementum non mollis justo aliquam.</p>
-                           </div>
-                           
-                           <div class="row">
-                              <div class="col-md-6 col-sm-6">
-                                 <div class="service">
-                                 <!-- Icon & title. Font Awesome icon used. -->
-                                 <h5><span><i class="fa fa-gift"></i> Web Designing</span></h5>
-                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vulputate eros nec odio egestas in dictum nisi vehicula. Pellentesque habitant fames ac turpis. </p>
-                                 </div>
-                              </div>
-                              
-                              <div class="col-md-6 col-sm-6">                           
-                                 <div class="service">
-                                 <!-- Icon & title. Font Awesome icon used. -->
-                                 <h5><span><i class="fa fa-cloud"></i> Web Development</span></h5>
-                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vulputate eros nec odio egestas in dictum nisi vehicula. Pellentesque habitant fames ac turpis. </p>
-                                 </div>
-                              </div>
-                           </div>
-                           
-                        </div>
-                     </div>
                      
                   </div>
                </div>
@@ -157,26 +36,6 @@
             
             
             <!-- Service ends -->
-            
-            <!-- CTA starts -->
-            
-            <div class="cta">
-               <div class="row">
-                  <div class="col-md-9">
-                     <!-- First line -->
-                     <p class="cbig">Lorem ipsum consectetur dolor sit amet, consectetur adipiscing.</p>
-                     <!-- Second line -->
-                     <p class="csmall">Duis vulputate consectetur malesuada eros nec odio consect eturegestas et netus et in dictum nisi vehicula.</p>
-                  </div>
-                  <div class="col-md-2">
-                     <!-- Button -->
-                     <div class="button"><a href="#">Get A Free Trail</a></div>
-                  </div>
-               </div>
-            </div>
-            
-            <!-- CTA Ends -->
-            
          </div>
       </div>
    </div>

67e0f243596659d737d079288a81f4c1d0036e6f
Jesus Flores <joy_warmgun@hotmail.com>
2019-01-22 11:42:12 -0600

    Cambiando ligas de practicas  On branch master 	modified:   src/app/views/practices.blade.php

--- src/app/views/practices.blade.php
+++ src/app/views/practices.blade.php
@@ -29,43 +29,43 @@
 				 <h2><a href="#">Práctica: Georreferenciación de imágenes satelitales mediante QGIS</a></h2>
 
                                  <p>En muchas ocasiones las imágenes que poseen información geográfica no cuentan con información georreferenciada, es decir, no cuentan con un sistema de referencia espacial, lo cual es requerido para tener la capacidad de localizar de manera precisa las entidades geográficas y obtener un correcto análisis de datos espaciales. Para ello es necesario conocer el proceso de georreferenciación cuya función es dotar a una imagen con un sistema de referencia espacial, además de corregir geométricamente la posición de los píxeles.</p>
-                                 <div class="button"><a target="_blank" href="practices/Coberturasforestales.pdf">Ver</a></div>
+                                 <div class="button"><a target="_blank" href="practices/1.1Practica_Georreferenciación_de_imagenes_mediante_QGIS_SetPassword.pdf">Ver</a></div>
                               </div>
                               <div class="entry">
 				 <h2><a href="#">Práctica: Comparación entre firmas espectrales realizadas con imágenes de satélite y con espectro radiómetro</a></h2>
 
                                  <p>La percepción remota estudia los objetos sin estar en contacto directo con ellos, para esto se vale de la radiación electromagnética que cruza la atmósfera e incide sobre los objetos, cada objeto tiene cierta huella de cómo se comporta al momento de interactuar con la radiación, a esta huella se le denomina respuesta espectral y se basa en los valores que el objeto reflecta, llamándose a este fenómeno reflectancias, es por ello que las respuestas espectrales están ligadas a las reflectancias de los objetos.</p>
-                                 <div class="button"><a target="_blank" href="practices/Coberturasforestales.pdf">Ver</a></div>
+                                 <div class="button"><a target="_blank" href="practices/1.2Firmas_espectrales_SetPassword.pdf">Ver</a></div>
                               </div>
                               <div class="entry">
 				 <h2><a href="#">Práctica: Análisis de datos de clorofila a partir de imágenes de satélite MODIS</a></h2>
 
                                  <p>La clorofila a, es un pigmento fotosintético que se encuentra en todo tipo de vegetación y en particular en el fitoplancton, permite a la plantas convertir luz solar en compuestos orgánicos mediante el proceso de la fotosíntesis, es base de la cadena alimenticia y un factor determinante en la producción pesquera; tiene mayor presencia en los organismos fotosintéticos y por ende se relaciona con la biomasa vegetal, al medir la concentración de la clorofila se puede determinar el deterioro o equilibrio del ecosistema.</p>
-                                 <div class="button"><a target="_blank" href="practices/Coberturasforestales.pdf">Ver</a></div>
+                                 <div class="button"><a target="_blank" href="practices/1.3Practica_clorofila_SetPassword.pdf">Ver</a></div>
                               </div>
                               <div class="entry">
 				 <h2><a href="#">Práctica: Coberturas Forestales</a></h2>
 
                                  <p>La cobertura forestal es una extensión de territorio ocupada por diferentes tipos de vegetación, la cual funge como indicador para el análisis y comportamiento de los bosques a lo largo del tiempo.</p>
-                                 <div class="button"><a target="_blank" href="practices/Coberturasforestales.pdf">Ver</a></div>
+                                 <div class="button"><a target="_blank" href="practices/1.4Coberturas_Forestales_SetPassword.pdf">Ver</a></div>
                               </div>
                               <div class="entry">
 				 <h2><a href="#">Práctica: Detección de cambios en cultivos utilizando imágenes de satélite</a></h2>
 
                                  <p>La percepción remota se ha convertido en una importante fuente de información en el manejo de la producción agrícola, no sólo a escalas locales sino a nivel global, particularmente para aquellas regiones en que el suministro de alimentos suele pasar por períodos críticos.</p>
-                                 <div class="button"><a target="_blank" href="practices/Coberturasforestales.pdf">Ver</a></div>
+                                 <div class="button"><a target="_blank" href="practices/1.5Deteccion_de_cambios_en_cultivos_utilizando_imagenes_de_satelite_SetPassword.pdf">Ver</a></div>
                               </div>
                               <div class="entry">
 				 <h2><a href="#">Práctica: Cálculo de suspensión de sedimentos</a></h2>
 
                                  <p>El monitoreo y el transporte de sedimentos en un río, implica realizar campañas de mediciones hidrológicas y sedimentológicas las cuales resultan costosas por la logística y equipo que requieren, este aspecto también limita el muestreo a sólo pocos sitios de interés.</p>
-                                 <div class="button"><a target="_blank" href="practices/Coberturasforestales.pdf">Ver</a></div>
+                                 <div class="button"><a target="_blank" href="practices/1.6Concentracion_de_sedimentos_suspendidos_SetPassword.pdf">Ver</a></div>
                               </div>
                               <div class="entry">
 				 <h2><a href="#">Práctica: Cálculo de la temperatura superficial del suelo en la Ciudad de México mediante Qgis</a></h2>
 
                                  <p>Esta práctica consiste en el cálculo de la temperatura superficial con Landsat 8. Por consiguiente, primeramente, se corrigen las imágenes, después se clasifican las cubiertas y se asignan las emisividades correspondientes y por último se calcula la temperatura.</p>
-                                 <div class="button"><a target="_blank" href="practices/Coberturasforestales.pdf">Ver</a></div>
+                                 <div class="button"><a target="_blank" href="practices/1.7Calculo_de_temperatura_sw_SetPassword.pdf">Ver</a></div>
                               </div>
                               <div class="clearfix"></div>
                               
@@ -83,43 +83,43 @@
 				 <h2><a href="#">Práctica: Análisis multicriterio para la creación de un eco parque en la localidad de Santa Elena, Yucatán</a></h2>
 
                                  <p>Se entiende por evaluación multicriterio (EMC) al conjunto de técnicas que están orientadas para la asistencia en la toma de decisiones, mediante un conjunto de operaciones espaciales teniendo como objetivo encontrar alternativas bajo diferentes criterios y objetivos considerando simultáneamente todas las variables que intervienen en un dado problema, para de esta forma, generar soluciones con los criterios establecidos desde un principio.</p>
-                                 <div class="button"><a target="_blank" href="practices/Coberturasforestales.pdf">Ver</a></div>
+                                 <div class="button"><a target="_blank" href="practices/2.1Practica_multicriterio_SetPassword.pdf">Ver</a></div>
                               </div>
                               <div class="entry">
 				 <h2><a href="#">Práctica: Elaboración de un modelo digital de elevación</a></h2>
 
                                  <p>La representación del relieve es importante para conocer información existente acerca de los elementos en la superficie de la Tierra, siendo de gran relevancia para el análisis y estudios de diversas índoles en los campos de la ingeniería y Ciencias de la Tierra.</p>
-                                 <div class="button"><a target="_blank" href="practices/Coberturasforestales.pdf">Ver</a></div>
+                                 <div class="button"><a target="_blank" href="practices/2.2Practica_MDE_SetPassword.pdf">Ver</a></div>
                               </div>
                               <div class="entry">
 				 <h2><a href="#">Práctica: Análisis de Trayectorias (Ruta Óptima)</a></h2>
 
                                  <p>Los Sistemas de Información Geográfica son herramientas útiles para la planificación, reducen tiempo, distancia y costo para la realización de trazados óptimos con su apoyo nos permite enfrentarnos a un aspecto restrictivo del territorio trazando la ruta óptima entre dos puntos para incurrir en el menor coste de esfuerzo durante nuestro desplazamiento. </p>
-                                 <div class="button"><a target="_blank" href="practices/Coberturasforestales.pdf">Ver</a></div>
+                                 <div class="button"><a target="_blank" href="practices/2.3Practica_Ruta_Optima_SetPassword.pdf">Ver</a></div>
                               </div>
                               <div class="entry">
 				 <h2><a href="#">Práctica: Creación de una página web utilizando QGIS</a></h2>
 
                                  <p>Al generar nuevos datos geográficos, es de suma importancia la manera que se distribuye, sea de manera física (Impreso), digital, o en aplicaciones web. Este tipo de procesos puede reducir el trabajo dando a conocer datos, análisis, datos geográficos, así como, características urbanas del proyecto.</p>
-                                 <div class="button"><a target="_blank" href="practices/Coberturasforestales.pdf">Ver</a></div>
+                                 <div class="button"><a target="_blank" href="practices/2.4Practica_mapa_web_SetPassword.pdf">Ver</a></div>
                               </div>
                               <div class="entry">
 				 <h2><a href="#">Práctica: Elaboración de una base de datos</a></h2>
 
                                  <p>La información Geoespacial es un factor fundamental en la toma de decisiones y en el desarrollo de la sociedad actual. Este tema es de importancia tanto para el sector público, el sector privado y académico, cualquier evento o fenómeno ocurre en un espacio determinado del planeta, por lo cual, es necesario conocer la ubicación espacial de cualquier fenómeno de interés, así como, las características de su entorno, dando como resultado tomar decisiones con mayor inteligencia, mayor eficiencia y a un menor costo.</p>
-                                 <div class="button"><a target="_blank" href="practices/Coberturasforestales.pdf">Ver</a></div>
+                                 <div class="button"><a target="_blank" href="practices/2.5Practica_BasededatosTERRSET_SetPassword.pdf">Ver</a></div>
                               </div>
                               <div class="entry">
 				 <h2><a href="#">Práctica: Creación de una geodatabase en PostgreSQL</a></h2>
 
                                  <p>La Geodatabase es un modelo que permite el almacenamiento físico de la información geográfica, ya sea en archivos dentro de un sistema de ficheros o en una colección de tablas en un Sistema Gestor de Base de Datos (Microsoft Access, Oracle, Microsoft SQL Server, IBM DB2 e Informix).</p>
-                                 <div class="button"><a target="_blank" href="practices/Coberturasforestales.pdf">Ver</a></div>
+                                 <div class="button"><a target="_blank" href="practices/2.6Practica_geodatabase_SetPassword.pdf">Ver</a></div>
                               </div>
                               <div class="entry">
 				 <h2><a href="#">Práctica: Geolocalización de sitios arqueológicos</a></h2>
 
                                  <p>En muchas ocasiones se necesita tener geolocalizados diferentes sitios, para manipularlos ya sea ingresarlos a algún mapa o aplicación, pero esta información solo está en imágenes, no contando con coordenadas propias, y es necesario obtener un punto, para eso se lleva a cabo la digitalización, esta consiste en convertir los puntos en un formato digital, que en este caso es hacer un shapefile del punto deseado, para tenerlo como elemento, de esta forma el sitio tendrá coordenadas y se podrá editar para asignarle otros atributos.</p>
-                                 <div class="button"><a target="_blank" href="practices/Coberturasforestales.pdf">Ver</a></div>
+                                 <div class="button"><a target="_blank" href="practices/2.7Practica_geolocalizacion_SetPassword.pdf">Ver</a></div>
                               </div>
                               <div class="clearfix"></div>
                               
